3bd3496141ab3373bd8e2a3f9ce5a14859ecea78
Morton Potash <mortonpotash@gmail.com>
2020-07-06 05:59:59 +0200

    edit on PC to test push

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,7 @@ export default function About() {
       <Header headerText="Ok, it's pretty cool, as you say." />
       <Header headerText="Morton! " headerSubText="Your Are Experimenting again" />
       <p>Such wow. Very React.</p>
+      <p>Instead of using ResilioSync, use github clone to recreate this tutorial on a different computer. (I'm writing from a Windows 10 PC). Use <code>git clone</code> with the link which you find on the github page for that project.</p>
     </div>
   )
 }
